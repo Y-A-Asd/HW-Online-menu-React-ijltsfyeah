e8dd30b7a63c027f1c7fab4d9cf9030e502c492a
yousof <yosofasady2@gmail.com>
2024-12-09 05:48:17 +0330

    feat: adding foods data and some slight design fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { Flip, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
+  const notify = () => toast("Wow so easy!");
   return (
-    <>
-      
-    </>
-  )
+    <div className="min-h-screen">
+      <Header />
+      <div>
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={true}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+          transition={Flip}
+        />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
