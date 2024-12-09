388f8aa5b7f9788de1da409379b4fa8fa59b3941
yousof <yosofasady2@gmail.com>
2024-12-09 05:47:36 +0330

    feat: addin header and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export function Footer() {
+  return (
+    <>
+      <div className="bg-amber-600 flex justify-center py-5 sticky px-7 top-[100vh] w-full ">
+        <p className="text-white text-sm font-allfont font-bold">
+          Made with ❤️ by{" "}
+          <span className="text-amber-900 font-extrabold text-md">
+            Flatlife
+          </span>
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+export function Header() {
+  return (
+    <>
+      <div className="bg-amber-500 flex justify-between py-5 px-7 sticky">
+        <h3 className="text-white font-extrabold text-2xl mx-4 font-farsi">
+          فست فود آنلاین
+        </h3>
+        <h4 className="text-white font-extrabold text-md mx-4 mt-1 font-farsi">
+          بهترین کیفیت را با ما تجربه کنید!
+        </h4>
+      </div>
+    </>
+  );
+}
